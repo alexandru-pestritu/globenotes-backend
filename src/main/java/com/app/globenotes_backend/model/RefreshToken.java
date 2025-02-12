a006812d5475d37c446fc94e191b72f94e4cda8e
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-12 21:49:06 +0200

    RefreshToken model and repository

---
+++ src/main/java/com/app/globenotes_backend/model/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.app.globenotes_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now(ZoneOffset.UTC);
+    }
+
+}
