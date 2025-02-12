97691eaf50f9217cba52f32f1765b41548cfa2c0
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-12 21:49:53 +0200

    JwtAuthenticationFilter and SecurityConfig

---
+++ src/main/java/com/app/globenotes_backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.app.globenotes_backend.security;
+
+
+import com.app.globenotes_backend.util.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authHeader.substring(7);
+
+            if (!jwtUtils.isTokenValid(token)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            Long userId = jwtUtils.getUserIdFromToken(token);
+            String email = jwtUtils.getEmailFromToken(token);
+
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(
+                            email,
+                            null,
+                            null
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        } catch (Exception e) {
+            log.error("JWT parse error: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
