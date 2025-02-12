8f4faaf5e13f97f94a496fb2ded690b89b74c260
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-12 12:26:37 +0200

    Initial commit

---
+++ src/main/java/com/example/globenotes_backend/GlobenotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.globenotes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobenotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobenotesBackendApplication.class, args);
+	}
+
+}
