fecbbafbf102abdac2456d37d336075d094db81c
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-13 13:54:43 +0200

    Added Swagger dependency and config file

--- src/main/java/com/app/globenotes_backend/config/SecurityConfig.java
+++ src/main/java/com/app/globenotes_backend/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
---
+++ src/main/java/com/app/globenotes_backend/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.app.globenotes_backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GlobeNotes API")
+                        .version("1.0.0")
+                        .description("Documentation for GlobeNotes API"));
+    }
+}
