2fab07eef3f58abbd5753559bfe2d9dabd62cecd
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-02-27 17:46:03 +0200

    Added aws s3 configuration

--- src/main/java/com/app/globenotes_backend/config/AwsConfig.java
+++ src/main/java/com/app/globenotes_backend/config/AwsConfig.java
@@ -1,19 +1,41 @@
 package com.app.globenotes_backend.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.ses.SesClient;
 
 @Configuration
 @Slf4j
 public class AwsConfig {
+    @Value("${aws.region}")
+    private String awsRegion;
 
     @Bean
     public SesClient sesClient() {
         return SesClient.builder()
                 .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(awsRegion))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(awsRegion))
                 .build();
     }
 }
